7f1c6ff3d2f666c0af8b334357b13ee7f46b0507
shinji0320 <shinji.blue.one@gmail.com>
2021-04-06 13:44:37 +0900

    onSubmit追加しました

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
-import React, { useState } from 'react';
-import jsonplaceholder from './apis/jsonplaceholder'
-import Button from './components/Button'
-import Resources from './components/Resources'
+import React from 'react'
+import SearchBar from './components/SearchBar'
+
 
 const App = () => {
-  const [resources, setResources] = useState([]);
-
-  const getPosts = async () => {
-    try {
-      const posts = await jsonplaceholder.get('/posts');
-        setResources(posts.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const getAlbums = async () => {
-    try {
-      const albums = await jsonplaceholder.get('/albums');
-        setResources(albums.data);
-    } catch (err) {
-      console.log(err);
-    }
+  const onSearchSubmit = (term) => {
+    console.log(term);
   };
   return (
-    <div>
-      <Button onClick={getPosts} text='テキスト'/>
-      <Button onClick={getAlbums} text='アルバム'/>
-      <Resources resources={resources}/>
+    <div className='ui container' style={{ marginTop: '20px' }}>
+      <SearchBar onSubmit={onSearchSubmit}/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
